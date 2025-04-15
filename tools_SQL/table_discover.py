9a63075029d2c686bc3b5e7b10a3f9779536cc87
migu_cheng <ianlk11234s@gmail.com>
2025-04-15 18:19:39 +0800

    增加 SQL tool 的分類，與一個初步的構想

---
+++ tools_SQL/table_discover.py
@@ -0,0 +1,20 @@
+# 這裡負責生成要探索 table 的 SQL 語法
+"""""
+目前會想要包含以下內容：
+- 填入 table 名稱
+- 放上想要特別 group by 確認的欄位，自動產生語法（或是用勾選的）
+- 查看特定 id 或是查看特定店家的資料
+
+"""""
+
+# 探索 table 的 SQL 語法
+sql_query = """
+SELECT 
+    field,
+    count(*) AS total_count,
+    COUNT(DISTINCT id) AS distinct_count
+FROM stg_salesforce__lead_history
+GROUP BY 1
+"""
+
+# 執行 SQL 語法
